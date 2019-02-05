433bed6ffce238e3575d188c24cee8319eb8fb7a
emilte <emilte@stud.ntnu.no>
2019-02-05 23:31:55 +0100

    Sync. Mostly update function for songs.txt

--- songs/views.py
+++ songs/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.http import JsonResponse
 from songs.forms import *
 from django.db.models import Q
+import json
 #from django.views import generic
 #from django.contrib.auth.decorators import login_required
 
@@ -27,6 +28,32 @@ def search_song_filter(form, queryset):
         queryset = queryset.filter(bpm__lte=max_bpm)
 
     return queryset
+
+def update_songs_txt(song, title=None):
+    # Add:
+    if title==None:
+        print("Add")
+        with open('songs/static/songs/songs.txt', mode='a', encoding="UTF-8") as songs:
+            tags = song.tags.values_list('name')
+            tags = [t[0] for t in tags]
+            song = song.__dict__
+            songs.write("\n{0}: {{'artist': '{1}', 'bpm': '{2}', 'tags': {3}, 'spotify': '{4}', 'URI': {5} }}".format(song['title'], song['artist'], song['bpm'], tags, song['spotify'], song['URI']))
+    # Update line:
+    else:
+        print("Update")
+        with open('songs/static/songs/songs.txt', mode='w+', encoding="UTF-8") as songs:
+            data = songs.readlines()
+            print(type(data))
+            print(data)
+            for i in range(len(data)):
+                print(title in data[i])
+                if title in data[i]:
+                    print('YES')
+                    tags = song.tags.values_list('name')
+                    tags = [t[0] for t in tags]
+                    song = song.__dict__
+                    data[i] = "\n{0}: {{'artist': '{1}', 'bpm': '{2}', 'tags': {3}, 'spotify': '{4}', 'URI': {5} }}".format(song['title'], song['artist'], song['bpm'], tags, song['spotify'], song['URI'])
+
 # End: Functions ---------------------------------------------------------------
 
 # Create your views here.
@@ -34,25 +61,28 @@ def home(request):
     return render(request, 'songs/home.html')
 
 def add_song(request):
+    form = SongForm()
     if request.method == 'POST':
         form = SongForm(request.POST)
         if form.is_valid():
-            form.save()
+            song = form.save()
+            update_songs_txt(song)
             return redirect('home')
-    # GET
-    else:
-        form = SongForm()
+
     return render(request, 'songs/song_form.html', {'form': form})
 
 def edit_song(request, songID):
     song = Song.objects.get(id=songID)
+    prev_song = song.title
     form = SongForm(instance=song)
     if request.method == 'POST':
         form = SongForm(request.POST, instance=song)
         if form.is_valid():
-            form.save()
+            song = form.save()
+            update_songs_txt(song, prev_song)
             return redirect('home')
     # GET or form failed
+
     return render(request, 'songs/song_form.html', {'form': form})
 
 def all_songs(request):
