f3028e58c2186179833a511fdd393341985301b1
emilte <emil.telstad@gmail.com>
2019-09-22 03:08:30 +0200

    Change static to staticroot

--- music_library/settings.py
+++ music_library/settings.py
@@ -27,10 +27,10 @@ ALLOWED_HOSTS = []
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticroot')
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "mediaroot")
 MEDIA_URL = '/media/'
 
 LOGIN_REDIRECT_URL = '/account/profile'
