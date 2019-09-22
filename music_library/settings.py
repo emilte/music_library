f0d74a0ae3b6000f74599cfe09a5a1f294fa2ff9
emilte <emil.telstad@gmail.com>
2019-09-22 03:55:09 +0200

    Setup heroku

--- music_library/settings.py
+++ music_library/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -177,7 +178,8 @@ except:
 
 
 
-
+# Activate Django-Heroku.
+django_heroku.settings(locals())
 
 
 checklist = {
