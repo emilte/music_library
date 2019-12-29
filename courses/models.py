1f75148149b9d5f9d3d9433ca8728354042f8adb
emilte <emil.telstad@gmail.com>
2019-12-29 03:00:26 +0100

    Fix str

--- courses/models.py
+++ courses/models.py
@@ -16,8 +16,7 @@ class Course(models.Model):
     tags = models.ManyToManyField('videos.VideoTag')
 
     def __str__(self):
-        return "course"
-        # return "{} ({})".format(self.getTitle(), self.getDate())
+        return "{} ({})".format(self.getTitle(), self.getDate())
 
     def getTitle(self):
         return self.title or None
@@ -45,7 +44,6 @@ class Section(models.Model):
     description = models.TextField(null=True, blank=True, default="")
     start = models.TimeField(null=True, blank=True)
     duration = models.IntegerField(null=True, blank=False)
-    #varighet2 = models.DurationField(null=True, blank=True)
     course = models.ForeignKey('courses.Course', on_delete=models.CASCADE, null=True, blank=True, related_name="sections")
     song = models.ForeignKey('songs.Song', on_delete=models.SET_NULL, null=True, blank=True, related_name="sections")
     video = models.ForeignKey('videos.Video', on_delete=models.SET_NULL, null=True, blank=True, related_name="sections")
@@ -54,8 +52,7 @@ class Section(models.Model):
         ordering = ['nr']
 
     def __str__(self):
-        return "section"
-        # return "Section ({}) in course: {}".format(self.getNr(), self.getCourse())
+        return "Section ({}) in course: {}".format(self.getNr(), self.getCourse())
         #return "{} - {}...".format(self.course.title[0:40], self.text[:40])
         #return "{} - ({}) {}...".format(self.course.title[0:40], self.nr, self.text[:40])
 
