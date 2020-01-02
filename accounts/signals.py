2ed1c55971cb46716d3fe83512bc9b826e51b758
emilte <emil.telstad@gmail.com>
2020-01-02 19:03:46 +0100

    Fix import

--- accounts/signals.py
+++ accounts/signals.py
@@ -1,9 +1,8 @@
 # imports
+from django.dispatch import receiver
 from django.db.models.signals import post_save
 from django.contrib.auth import get_user_model
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from users import models as user_models
+from accounts import models as account_models
 
 User = get_user_model()
 
@@ -12,5 +11,5 @@ User = get_user_model()
 @receiver(post_save, sender=User)
 def create_settings(sender, instance, created, **kwargs):
     if created:
-        user_models.Settings.objects.create(user=instance)
+        account_models.Settings.objects.create(user=instance)
         print("== (users.signals.py) <Settings> created for <{}> ==".format(instance))
