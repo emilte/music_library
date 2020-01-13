c8a41310fadc37f30fe3884653c86e3b923e8dad
emilte <emil.telstad@gmail.com>
2020-01-13 14:42:41 +0100

    modify django-seed init

--- music_library/management/commands/heroku_release.py
+++ music_library/management/commands/heroku_release.py
@@ -18,6 +18,15 @@ class Command(BaseCommand):
         management.call_command('migrate')
 
         try:
+            import fileinput
+            filename = "/app/.heroku/python/lib/python3.8/site-packages/django_seed/__init__.py"
+
+            with fileinput.FileInput(filename, inplace=True, backup='.bak') as file:
+                for line in file:
+                    origin = "cls.fakers[code].seed(random.randint(1, 10000))"
+                    replacement = "cls.fakers[code].seed_instance(random.randint(1, 10000))"
+                    print(line.replace(origin, replacement), end='')
+
             management.call_command('myseed')
         except Exception as e:
             print(e)
